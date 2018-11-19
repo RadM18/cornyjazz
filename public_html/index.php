dd48b1280073d19a06ed985399f81c7490334c93
XpertMAN <37657232+XpertMAN@users.noreply.github.com>
2018-11-19 15:13:18 +0300

    cornyhazz
    cornyhazz

---
+++ public_html/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+	<link rel="shortcut icon" href="images/favicon.png">
+    <title>cornyTASTEdrive</title>
+    <meta name="description" content="УЧАСТВУЙ В КОНКУРСЕ cornyTASTEdrive. ПОЛУЧИ В ПОДАРОК МЕСЯЧНЫЙ ЗАПАС CORNY!">
+  	<meta name="Keywords" content="УЧАСТВУЙ В КОНКУРСЕ cornyTASTEdrive">
+  	<meta name="author" content="">
+	<link href="css/fonts.css?v=1" rel="stylesheet">
+    <link href="css/site.css?v=1" rel="stylesheet">
+  </head>
+<body id="page-home">
+<div class="container">
+			<div class="main">
+				<img class="img-fluid" src="images/Corny_Big.png">
+				<p style="margin-bottom: 1.2em;margin-top: 0;"><span>УЧАСТВУЙ В КОНКУРСЕ cornyTASTEdrive</span></p>
+				Найди арт-объект Corny на фестивале Geek Picnic.<br /><br />
+				Выложи фото с ним в Инстаграм с хэштегом <b>#cornytastedrive</b> в день фестиваля.<br /><br />
+				Набери больше всего лайков.<br />
+				<p style="margin-bottom: 1.2em;margin-top:1.2em;"><span>ПОЛУЧИ В ПОДАРОК МЕСЯЧНЫЙ ЗАПАС CORNY!</span>
+				<p>Подробные правила конкурса на <a href="Правила Акции - CornyTasteDrive.pdf" target="_blank">ссылке</a>
+				<p><br /><small>*По данным ООО «ГфК-Русь», Сorny — лидер по объемам продаж (кг) в категории «Злаковые батончики» в 2017 году в России.</small>
+     </div>
+</div>
+</body></html>
